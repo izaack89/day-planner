699edb8f617ea6d5d32daadad7f5de96a9b54490
German Ramirez <german@sidewalk.ai>
2021-04-10 12:26:35 -0500

    Deleted the console logs that I used to track the issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@ var timeBlocks = $('.time-block')
 var momentInstance = moment();
 var currentDate = momentInstance.format('dddd, MMM Do YYYY');
 var currentHour  = momentInstance.format('H');
-console.log(currentDate, currentHour);
 
 $(".saveBtn").on("click", function () {
     var timeBlockId = $(this).parent().attr("id");
@@ -14,9 +13,7 @@ $(".saveBtn").on("click", function () {
 $.each( timeBlocks, function( key, timeBlock ) {
     var hourCheck = parseInt(timeBlock.id.replace('hour', ''));
     var timeBlockId = timeBlock.id;
-    var textArea = $('#' + timeBlockId).find('textarea');
-    console.log($('#'+timeBlockId).find('textarea'));
-    console.log(key, timeBlock.id, hourCheck, currentHour);
+    var textArea = $('#' + timeBlockId).find('textarea');    
     textArea.val(localStorage.getItem(timeBlockId));
     if (currentHour == hourCheck) {
         $('#'+timeBlockId).addClass('present');
