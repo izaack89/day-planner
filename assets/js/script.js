5fd1492369bf5729265af943c04ee890fd356585
German Ramirez <german@sidewalk.ai>
2021-04-10 11:45:43 -0500

    Added the code that will get the data from localstorage and will set that into the textarea

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,16 @@ var timeBlocks = $('.time-block')
 var momentInstance = moment();
 var currentDate = momentInstance.format('dddd, MMM Do YYYY');
 var currentHour  = momentInstance.format('H');
-console.log(currentDate,currentHour) 
+console.log(currentDate, currentHour);
+
+
 $.each( timeBlocks, function( key, timeBlock ) {
     var hourCheck = parseInt(timeBlock.id.replace('hour', ''));
     var timeBlockId = timeBlock.id;
     var textArea = $('#' + timeBlockId).find('textarea');
     console.log($('#'+timeBlockId).find('textarea'));
-    console.log(key, timeBlock.id,hourCheck,currentHour);
+    console.log(key, timeBlock.id, hourCheck, currentHour);
+    textArea.val(localStorage.getItem(timeBlockId));
     if (currentHour == hourCheck) {
         $('#'+timeBlockId).addClass('present');
     } else if (currentHour > hourCheck) {        
